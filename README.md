ec5f679159672f8ea7da2dae4d56a1edeb01c093
Jeffrey NGUYEN <jeffrey69140@gmail.com>
2022-07-15 14:55:04 +0200

    Ajout de la version finale

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # WaTOR
+
+README
+
+# Programmation du Projet WATOR
+
+Le Projet WaTOR est un programme qui permet de visualiser une simulation d'un ecosystème entre poisson et requins vivant dans un espace torique. 
+
+## Pré-Requis 
+
+-Python
+
+## Démarrage
+
+Exécutez le programme avec **VSCODE** , dans la partie initialisation vous pouvez choisir le nombre de requins et de poissons, le temps de reproduction,l'énergie des requins et des poissons ainsi que la taille de l'environnement.
+Vous pouvez également choisir le nombre de tour que vous voulez faire.
+
+## Problèmes
+
+Pour faire jouer nos requins ou nos poissons on va parcourir notre grille et les faires bouger de ligne en ligne. Certaines créatures peuvent donc bouger plus d'une fois dans un tour de jeu. 
+Les requins n'ont pas d'énergie limite à partir du moment ou ils mangent un poisson il gagne +2 d'énergie.
+L'écosystème a tendance à pencher en faveur des requins.
+
+
+## Fabriqué avec
+
+- **Visual Studio Code** - Editeur de code
+- **Git** - Logiciel de gestion.
+- **GitHub** - Site web et service de cloud pour stocker et gérer le code.
+- **Python** - Langage de programmation interprété.
+
+## Versions
+
+Dernière version : 1.0
+
+## Auteurs
+
+- Jeffrey NGUYEN 
+- Yi ZHANG
+- Omar SAHBOUN
+
+
--- wator 2.0.py
+++ wator 2.0.py
@@ -12,7 +12,9 @@ class Fish:
             parameters : self, grille qui est un attribut grille d'un objet de type Aquartorium
             cette méthode va nous renvoyer une liste des déplacements possibles 'return liste'
             cette méthode contient 4 variables Nord,Sud,Ouest,Est qui represente la position x+1 x-1 y+1 y-1 ces variables sont créer avec des modulos pour que les déplacements
-            soit bien dans une grille torique , une variable n qui va s'incrémenter de 1 peu importe et qui va s'arreter après qu'elle est vérifier les 4 positions
+            soit bien dans une grille torique , une variable n qui va s'incrémenter de 1 et qui va s'arreter après qu'elle est vérifier les 4 positions.
+            cette méthode retourne 2 liste soit une liste de mouvement de disponible : liste , soit une liste de 0 : liste_null pour dire qu'il n'y a aucun mouvement
+            elle retourne en priorité la liste de mouvement si sa len est supérieur à 0.
             """
             Nord = (self.y-1)%monde.ordonne
             Sud = (self.y+1)%monde.ordonne
@@ -49,13 +51,13 @@ class Fish:
             if len(liste)>0:
                 return liste
             else : return liste_null
-    def fish_move(self,monde,nRandom):
+    def fish_move(self,monde,nRandom,fish_time):
             """création de la méthode de déplacement du poisson 
             parameters : self , grille qui est un attribut grille  d'objet de type Aquartorium,nRandom qui est un int : nombre aléatoire qu'on va obtenir
-            En fonction de nRandom le poisson va bouger au nord,sud,ouest,est 
-            on créer 4 variables Nord,Sud,Ouest,Est qui represente la position x+1 x-1 y+1 y-1 ces variables sont créer avec des modulos pour que les déplacements
+            En fonction de nRandom le poisson va bouger au nord,sud,ouest,est et time qui est le temps de reproduction du poisson.
+            on créer 4 variables Nord,Sud,Ouest,Est qui represente la position  y+1 y-1 x+1 x-1 ces variables sont créer avec des modulos pour que les déplacements
             soit bien dans une grille torique
-            si le temps de reproduction du poisson est égale à 5 (d'ou le reproduction%5==0) on enlever son temps de reproduction initiale et on créer un poisson
+            si le temps de reproduction du poisson est supérieur à time on enlever son temps de reproduction initiale et on créer un poisson
             si il est inférieur à 5 on bouge notre poisson est la case d'avant est égale à none
             """
             Nord = (self.y-1)%monde.ordonne
@@ -68,7 +70,7 @@ class Fish:
                 monde.grille[self.y][self.x]=monde.grille[self.y][self.x]
             if nRandom == 1 :
                 if monde.grille[Nord][self.x]== None:
-                    if self.reproduction_time > 4:
+                    if self.reproduction_time > fish_time:
                         self.reproduction_time = 0
                         self.y = Nord 
                         monde.grille[Nord][self.x]= self
@@ -82,7 +84,7 @@ class Fish:
                         
             if nRandom == 2:
                 if monde.grille[Sud][self.x]== None:
-                    if  self.reproduction_time > 4 :
+                    if  self.reproduction_time > fish_time :
                         self.reproduction_time=0
                         self.y = Sud
                         monde.grille[Sud][self.x]=self
@@ -95,7 +97,7 @@ class Fish:
                         
             if nRandom == 3:
                 if monde.grille[self.y][Ouest]== None:
-                    if self.reproduction_time > 4 :
+                    if self.reproduction_time > fish_time :
                         self.reproduction_time = 0
                         self.x = Ouest
                         monde.grille[self.y][Ouest]=self
@@ -107,7 +109,7 @@ class Fish:
                         monde.grille[oldypos][oldxpos]=None
             if nRandom == 4:
                 if monde.grille[self.y][Est]== None:
-                    if self.reproduction_time > 4 :
+                    if self.reproduction_time > fish_time :
                         self.reproduction_time = 0
                         self.x = Est
                         monde.grille[self.y][Est]=self
@@ -118,12 +120,12 @@ class Fish:
                         monde.grille[self.y][Est]=Fish(self.reproduction_time,Est,self.y)
                         monde.grille[oldypos][oldxpos]= None
                         
-    def fish_play(self,monde):
+    def fish_play(self,monde,fish_time):
         nRandom = 0
         liste = []
         liste = self.fish_movements(monde)
         nRandom=choice(liste)
-        self.fish_move(monde,nRandom)
+        self.fish_move(monde,nRandom,fish_time)
 class Shark(Fish):
 
     def __init__(self,reproduction_time,x,y,energy):
@@ -132,8 +134,12 @@ class Shark(Fish):
     def shark_movements(self,monde):
         """ méthode qui renvoie deux listes de déplacements possible une qui contient que des cases none et une autre où il y'a des poissons 
         on renvoit la liste cases où il y'a des poissoins en priorité
-        on créer 4 variables Nord,Sud,Ouest,Est qui represente la position x+1 x-1 y+1 y-1 ces variables sont créer avec des modulos pour que les déplacements
+        on créer 4 variables Nord,Sud,Ouest,Est qui represente la position  y+1 y-1 x+1 x-1 ces variables sont créer avec des modulos pour que les déplacements
         soit bien dans une grille torique
+        une variable n qui va s'incrémenter de 1 et qui va s'arreter après qu'elle est vérifier les 4 positions.
+        cette méthode retourne 3 liste soit une liste de mouvement de disponible contenant les cases vides : liste , soit une liste de 0 : liste_null pour dire qu'il n'y a aucun mouvement
+        soit une liste qui contient la liste des mouvements ou les cases contiennent des poissons : liste_cases_poissons
+        cette méthode retourne priorité liste_cases_poisson après liste après liste_null
 
         """
         Nord = (self.y-1)% monde.ordonne
@@ -188,7 +194,7 @@ class Shark(Fish):
             return liste
         else : 
             return liste_null
-    def shark_move(self,monde,nRandom):
+    def shark_move(self,monde,nRandom,shark_time):
         """création de la méthode de déplacement du requin
         parameters : self , monde.grille  qui est un attribut monde.grille de l'objet de type Aquartorium,nRandom qui est un int : nombre aléatoire qu'on va obtenir
         En fonction de nRandom le poisson va bouger au nord,sud,ouest,est 
@@ -218,7 +224,7 @@ class Shark(Fish):
                 monde.grille[self.y][self.x] = None  
             elif monde.grille[Nord][self.x]== None:
 
-                if self.reproduction_time > 6 :   
+                if self.reproduction_time > shark_time :   
                     self.reproduction_time =0
                     self.y=Nord
                     monde.grille[Nord][self.x]= self
@@ -231,7 +237,7 @@ class Shark(Fish):
             elif type(monde.grille[Nord][self.x])==Fish:
                 
                 
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.energy += 2
                     self.y = Nord
@@ -250,7 +256,7 @@ class Shark(Fish):
             elif monde.grille[Sud][self.x]== None:
 
                 
-                if self.reproduction_time > 6  : 
+                if self.reproduction_time > shark_time  : 
                     self.reproduction_time =0
                     self.y = Sud
                     monde.grille[Sud][self.x]= self
@@ -263,7 +269,7 @@ class Shark(Fish):
             elif type(monde.grille[Sud][self.x]) ==Fish:
                 
                
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.energy +=2
                     self.y=Sud
@@ -285,7 +291,7 @@ class Shark(Fish):
 
             elif monde.grille[self.y][Ouest]== None:
 
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.x = Ouest
                     monde.grille[self.y][Ouest]=self
@@ -298,7 +304,7 @@ class Shark(Fish):
                         monde.grille[oldypos][oldxpos]= None
             elif type (monde.grille[self.y][Ouest])==Fish:
 
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.energy += 2
                     self.x = Ouest
@@ -318,7 +324,7 @@ class Shark(Fish):
                 monde.grille[self.y][self.x] = None
             elif monde.grille[self.y][Est]== None:
                 
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.x = Est
                     monde.grille[self.y][Est]=self
@@ -330,7 +336,7 @@ class Shark(Fish):
                     monde.grille[oldypos][oldxpos]= None
             elif type(monde.grille[self.y][Est]) == Fish:
                 
-                if self.reproduction_time > 6:
+                if self.reproduction_time > shark_time:
                     self.reproduction_time =0
                     self.energy+=2
                     self.x = Est
@@ -343,12 +349,12 @@ class Shark(Fish):
                     monde.grille[self.y][Est]= self
                     monde.grille[oldypos][oldxpos] = None
                     
-    def shark_play(self,monde):
+    def shark_play(self,monde,shark_time):
         nRandom = 0
         liste = []
         liste = self.shark_movements(monde)
         nRandom=choice(liste)
-        self.shark_move(monde,nRandom)
+        self.shark_move(monde,nRandom,shark_time)
 
         
 class Aquartorium:
@@ -377,7 +383,7 @@ class Aquartorium:
                 elif j is None:
                     ligne_aff += " 0 "
             print(ligne_aff)
-    def peupler(self,sharks_number,fishes_number):
+    def peupler(self,sharks_number,fishes_number,energy):
             """ parameters : nb de requins et nb de poisson 
             elle va remplir notre grille de poissons et de requins dans une
             """
@@ -387,7 +393,7 @@ class Aquartorium:
                 while self.grille[rdnumbery][rdnumberx] is not None:
                     rdnumberx = randint(0,self.abscisse-1)
                     rdnumbery= randint(0,self.ordonne-1)
-                self.grille[rdnumbery][rdnumberx]=Shark(0,rdnumberx,rdnumbery,20)
+                self.grille[rdnumbery][rdnumberx]=Shark(0,rdnumberx,rdnumbery,energy)
             for i in range(fishes_number):
                 rdnumberx = randint(0,self.abscisse-1)
                 rdnumbery= randint(0,self.ordonne-1)
@@ -395,26 +401,66 @@ class Aquartorium:
                     rdnumberx = randint(0,self.ordonne-1)
                     rdnumbery= randint(0,self.abscisse-1)
                 self.grille[rdnumbery][rdnumberx]=Fish(0,rdnumberx,rdnumbery)
-    def jouer_tour(self):
+    def jouer_tour(self,shark_time,fish_time):
         """la fonction jouer tour ne prend aucun parametre et fait jouer un tour c'est à dire qu'elle va faire bouge tous les créatures dans la grille
         """
         for abscisse in self.grille:
             for ordonne in abscisse:
                 if type(ordonne) ==Fish:
-                    ordonne.fish_play(self)  
+                    ordonne.fish_play(self,fish_time)  
                 elif type(ordonne) == Shark:
-                    ordonne.shark_play(self)
+                    ordonne.shark_play(self,shark_time)
             
 
+#####################################################
+#                   INITIALISATION                  #
+#####################################################
+abscisse = int(input("Enter la largueur de la grille  :  "))
 
-monde = Aquartorium(5,5)
-monde.peupler(1,4)     
-p=0
+while abscisse <2:
+    abscisse = int(input("Enter la largueur de la grille  :  "))
 
-nRandom = 0
-liste = []
-while p<20:
-    monde.jouer_tour()
+ordonne = int(input("Entrer la largueur de la grille  : "))    
+
+while ordonne <2:
+    ordonne = int(input("Entrer la largueur de la grille  : "))
+
+fishes_number = int(input("Entrer le nombre de poissons : "))
+sharks_number = int(input("Entrer le nombre de requins : "))
+
+while fishes_number and sharks_number == 0:
+    fishes_number = int(input("Entrer le nombre de poissons : "))
+    sharks_number = int(input("Entrer le nombre de requins : "))
+
+fish_time= int(input("Entrer le temps de reproduction des poissons : "))
+shark_time = int(input("Entrer le temps de reproduction des requins : "))
+
+energy = int(input("Entrez l'énergie des requins : "))
+
+while energy == 0:
+    energy = int(input("Entrez l'énergie des requins : "))
+
+while fish_time and shark_time == 0:
+    fish_time= int(input("Entrer le temps de reproduction des poissons : "))
+    shark_time = int(input("Entrer le temps de reproduction des requins : "))
+
+tour= int(input("Entrer le Nombre de tour que vous voulez faire : "))
+
+while tour <0 :
+    tour= int(input("Entrer le Nombre de tour que vous voulez faire : "))
+
+print("############################################################################")
+
+
+monde = Aquartorium(abscisse,ordonne)
+monde.peupler(sharks_number,fishes_number,energy)
+p =0
+#####################################################)
+#                   DEMARRAGE                       #
+#####################################################
+
+while p<tour:
+    monde.jouer_tour(shark_time,fish_time)
     monde.affiche_grille()
     "on affiche la grille à chaque fois qu'un tour complet de la grille à été effectuer : un requin ou un poisson peut bouger plusieurs fois"
     print("############################")
